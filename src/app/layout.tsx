55a8be830a94f8f855c0e1b7c6b1b9e248776065
Exosky12 <exoskyv2@gmail.com>
2023-05-30 09:36:00 +0200

    Fixes #1 [CONFIG]

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,6 @@
-import './globals.css'
-import { Inter } from 'next/font/google'
 
-const inter = Inter({ subsets: ['latin'] })
+import { ClerkProvider } from '@clerk/nextjs' 
+
 
 export const metadata = {
   title: 'Create Next App',
@@ -14,8 +13,10 @@ export default function RootLayout({
   children: React.ReactNode
 }) {
   return (
-    <html lang="en">
-      <body className={inter.className}>{children}</body>
-    </html>
+    <ClerkProvider>
+      <html lang="en">
+        <body>{children}</body>
+      </html>
+    </ClerkProvider>
   )
 }
