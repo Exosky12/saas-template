4b4cf97d9f54c82439f02c3c7e07f4e247c9daf7
Exosky12 <102408092+Exosky12@users.noreply.github.com>
2023-06-10 21:39:33 +0200

    Feature/nav bar (#15)
    * Closes #13 navBar done, responsive working. Optional:  Auth ui smoother, dark-light pre-placed
    
    * fix NavBar
    
    * Update NavBar.tsx
    
    * Delete package-lock.json
    
    * fix package
    
    * fix package

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,3 @@
-import {currentUser, UserButton} from "@clerk/nextjs";
-
 export default async function Home() {
-  const user = await currentUser()
-    console.log(user)
-  return (
-      <UserButton/>
-  )
+	
 }
