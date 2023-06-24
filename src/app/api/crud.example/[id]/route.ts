77ce5edabe15dccf2a5bc99ca102501ffbb1cd5a
AlexTraveylan <monsieurdemares@gmail.com>
2023-06-24 22:25:44 +0200

    service -> src

--- src/app/api/crud.example/[id]/route.ts
+++ src/app/api/crud.example/[id]/route.ts
@@ -1,35 +1,21 @@
 import { NextRequest, NextResponse } from "next/server";
-import { articleService } from "../../../../../services/article-example.service";
+import { articleService } from "../../../../services/article-example.service";
 
-export async function GET(
-  request: NextRequest,
-  { params }: { params: { id: string } }
-) {
+export async function GET(request: NextRequest, { params }: { params: { id: string } }) {
   const article = await articleService.get(params.id);
 
   return NextResponse.json({ article });
 }
 
-export async function PUT(
-  request: NextRequest,
-  { params }: { params: { id: string } }
-) {
+export async function PUT(request: NextRequest, { params }: { params: { id: string } }) {
   const { published } = await request.json();
 
-  const newArticle = await articleService.update(
-    params.id,
-    undefined,
-    undefined,
-    published
-  );
+  const newArticle = await articleService.update(params.id, undefined, undefined, published);
 
   return NextResponse.json(newArticle);
 }
 
-export async function DELETE(
-  request: NextRequest,
-  { params }: { params: { id: string } }
-) {
+export async function DELETE(request: NextRequest, { params }: { params: { id: string } }) {
   const newArticle = await articleService.delete(params.id);
 
   return NextResponse.json(newArticle);
--- src/app/crud/[id]/page.tsx
+++ src/app/crud/[id]/page.tsx
@@ -1,12 +1,5 @@
-import {
-  Card,
-  CardContent,
-  CardDescription,
-  CardFooter,
-  CardHeader,
-  CardTitle,
-} from "@/components/ui/card";
-import { articleService } from "../../../../services/article-example.service";
+import { Card, CardContent, CardDescription, CardFooter, CardHeader, CardTitle } from "@/components/ui/card";
+import { articleService } from "../../../services/article-example.service";
 
 async function getArticleExemple(params: { id: string }) {
   const articleExemple = await articleService.get(params.id);
@@ -18,20 +11,14 @@ async function getArticleExemple(params: { id: string }) {
   return articleExemple;
 }
 
-export default async function ArticlePage({
-  params,
-}: {
-  params: { id: string };
-}) {
+export default async function ArticlePage({ params }: { params: { id: string } }) {
   const article = await getArticleExemple(params);
 
   return (
     <Card className="w-[360px]">
       <CardHeader>
         <CardTitle>{article.title}</CardTitle>
-        <CardDescription>
-          Publié le {new Date(article.createdAt).toLocaleDateString()}
-        </CardDescription>
+        <CardDescription>Publié le {new Date(article.createdAt).toLocaleDateString()}</CardDescription>
       </CardHeader>
       <CardContent>
         <p className="text-center">{article.content}</p>
@@ -39,9 +26,7 @@ export default async function ArticlePage({
       <CardFooter className="flex flex-row justify-between">
         <p
           className={`px-2 inline-flex text-xs leading-5 font-semibold rounded-full ${
-            article.published
-              ? "bg-green-100 text-green-800"
-              : "bg-red-100 text-red-800"
+            article.published ? "bg-green-100 text-green-800" : "bg-red-100 text-red-800"
           }`}
         >
           {article.published ? "Publié" : "Non publié"}
